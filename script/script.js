e8f7ad74937afd931d453380166c3d8bc04a80e0
Maciej Preus <mpreus@onet.eu>
2020-01-27 12:21:43 +0100

    Add visual part of the calculator

--- script/script.js
+++ script/script.js
@@ -2,27 +2,37 @@ window.addEventListener("DOMContentLoaded", init);
 
 /* all keys needed in the project: */
 const opts = ["*", "/", "+", "-", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", "."];
-/* special oerations key: */
+/* special operations key: */
 const spec = ["*", "/", "+", "-",];
+
+/* function for buttons: */
+function addOutput(e) {
+	console.log(e);
+}
 
 function init() {
 	document.title = "JavaScript Calculator";
 
-/* container for the calculator */
+/* container for calculator */
 	const container = document.createElement("div");
 	container.classList.add("container");
 	container.style.maxWidth = "600px";
 	container.style.margin = "auto";
+	container.style.border = "1px solid grey";
+	container.style.borderRadius = "5px";
 	document.body.appendChild(container);
 
 /* output - space for results of calculations, placed in the 'container': */
 	const output = document.createElement("input");
 	output.setAttribute("type", "text");
 	output.classList.add("output");
-	output.style.width = "100%";
+	output.style.width = "98%";
 	output.style.lineHeight = "50px";
 	output.style.fontSize = "3em";
 	output.style.textAlign = "right";
+	output.style.margin = "3px";
+	output.style.border = "1px solig grey";
+	/*output.style.borderRadius = "3px";*/
 	container.appendChild(output);
 
 /* calculator's body element: */
@@ -31,7 +41,27 @@ function init() {
 	main.style.width = "100%";
 	container.appendChild(main);
 
+/* keys for calculator: */
+	opts.forEach(function(val) {
+		console.log(val);
+		btnMaker(val, addOutput);
+	});
 
+/* making buttons with their attributes... */
+	function btnMaker(txt, myFunction) {
+		let btn = document.createElement("button");
+		btn.setAttribute("type", "button");
+		btn.style.width = "23%";
+		btn.style.lineHeight = "50px";
+		btn.style.margin = "1%";
+		btn.style.fontSize = "2em";
+		btn.val = txt;
+		btn.textContent = txt;
+		/* ...their events... */
+		btn.addEventListener("click", myFunction);
+		/* ...and their place in the project: */
+		main.appendChild(btn);
+	}
 
 
 }
